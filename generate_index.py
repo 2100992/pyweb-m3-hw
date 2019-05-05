69c19f3163b6336961ea6ac9898f746844826fdb
Shvd <2100992@gmail.com>
2019-05-05 07:42:44 +0300

    2

--- generate_index.py
+++ generate_index.py
@@ -5,32 +5,41 @@ from datetime import datetime as dt
 
 def generate_head(title):
     head = f'''<head>
-    <meta charset = "utf-8">
-    <title>{title}</title>
-    <head>'''
+        <meta charset = "utf-8">
+        <title>{title}</title>
+        <head>'''
     return head
 
-def generate_body(header, paragraphs):
+def generate_body(header, paragraphs, footer):
     body = f'<h1>{header}</h1>'
     for i in paragraphs:
         body = f'''{body}
         <p>{i}</p>'''
-    return f'<body>{body}</body>'
+    return f'''<body>
+        {body}
+        {footer}
+        </body>'''
 
 def generate_page(head, body):
     page = f'''<html>
-    {head}
-    {body}
-    </html>
-    '''
+        {head}
+        {body}
+        </html>'''
     return page
 
-def save_page(title, header, paragraphs, output = 'index.html'):
+def generate_footer():
+    footer = '''<hr/>
+        <a href="about.html">
+            О чем эта страница
+        </a>'''
+    return footer
+
+def save_page(title, header, paragraphs, footer, output = 'index.html'):
     fp = open(output, 'w', encoding='utf8')
     #today = dt.now().date()
     page =  generate_page(
         head = generate_head(title),
-        body = generate_body(header = header, paragraphs = paragraphs)
+        body = generate_body(header = header, paragraphs = paragraphs, footer = footer)
         )
     print(page, file = fp)
     fp.close()
@@ -40,7 +49,8 @@ def main():
     save_page(
         title = 'Гороскоп на сегодня',
         header = 'Что день ' + str(today) + ' готовит',
-        paragraphs = generate_prophecies(),
+        paragraphs = generate_prophecies(total_num=3, num_sentences=4),
+        footer = generate_footer(),
     )
 
 main()
